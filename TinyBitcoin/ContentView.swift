6af8a1a72c2ea7a748f7707049c86f1feab981f1
Jacob Davis <ismyhc@gmail.com>
2022-06-12 12:35:03 -0400

    Initial Commit

---
+++ TinyBitcoin/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  TinyBitcoin
+//
+//  Created by Jacob Davis on 6/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TinyBitcoin/TinyBitcoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyBitcoinApp.swift
+//  TinyBitcoin
+//
+//  Created by Jacob Davis on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TinyBitcoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
